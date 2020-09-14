e96854c338a12821a668a858da2aec937558f144
AviNissimHart <ahart@qa.com>
2020-09-14 14:25:47 +0100

    first attempt at making order table

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,9 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Avi', 'Nissim');
+
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Chocolate', '0.5');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Birthday Cake', '20');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Coca Cola', '0.80');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Scented Candle', '2.50');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Bread', '0.90');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Broom', '4.99');
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -7,3 +7,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+
+USE `ims` ;
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NULL DEFAULT NULL,
+    `price` FLOAT NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+USE `ims` ;
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_customer_id` INT(11) NOT NULL,
+    `fk_item_id` INT(11) NOT NULL,
+    `item_name` VARCHAR(40) NULL DEFAULT NULL,
+    `price` FLOAT NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`fk_customer_id`) REFERENCES customers(`id`),
+    FOREIGN KEY (`fk_item_id`) REFERENCES items(`id`)
+);
