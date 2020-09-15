5b829b663657fb9d6cca25fd76fedcb0094794b7
AviNissimHart <ahart@qa.com>
2020-09-15 15:16:04 +0100

    alterations for the order table

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -8,8 +8,8 @@ INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Scented Candle', '2.50
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Bread', '0.90');
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Broom', '4.99');
 
-INSERT INTO `ims`.`orders` (`fk_customer_id`, `fk_item_id`) VALUES ('1', '1');
-INSERT INTO `ims`.`orders` (`fk_customer_id`, `fk_item_id`) VALUES ('1', '2');
-INSERT INTO `ims`.`orders` (`fk_customer_id`, `fk_item_id`) VALUES ('1', '3');
-INSERT INTO `ims`.`orders` (`fk_customer_id`, `fk_item_id`) VALUES ('1', '5');
-INSERT INTO `ims`.`orders` (`fk_customer_id`, `fk_item_id`) VALUES ('2', '5');
+INSERT INTO `ims`.`orders` (`fk_cid`) VALUES ('1');
+INSERT INTO `ims`.`orders` (`fk_cid`) VALUES ('1');
+INSERT INTO `ims`.`orders` (`fk_cid`) VALUES ('1');
+INSERT INTO `ims`.`orders` (`fk_cid`) VALUES ('1');
+INSERT INTO `ims`.`orders` (`fk_cid`) VALUES ('2');
