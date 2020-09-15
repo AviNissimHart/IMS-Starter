8d76df4e848a15a1b3873b85e7e40908d04ca777
AviNissimHart <ahart@qa.com>
2020-09-15 12:59:41 +0100

    adjustments for the new tables

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -22,13 +22,14 @@ CREATE TABLE IF NOT EXISTS `ims`.`orders` (
     `oid` INT(11) NOT NULL AUTO_INCREMENT,
     `fk_cid` INT(11) NOT NULL,
     PRIMARY KEY (`oid`),
-    FOREIGN KEY (`fk_cid`) REFERENCES customers(`cid`),
+    FOREIGN KEY (`fk_cid`) REFERENCES customers(`cid`)
 );
 
+USE `ims`;
 CREATE TABLE IF NOT EXISTS `ims`.`orderItems` (
     `oiid` INT(11) NOT NULL AUTO_INCREMENT,
     `fk_iid` INT(11) NOT NULL,
-    `fk_iid` INT(11) NOT NULL,
+    `fk_oid` INT(11) NOT NULL,
     `item_name` VARCHAR(40) NULL DEFAULT NULL,
     `price` FLOAT NULL DEFAULT NULL,
     PRIMARY KEY (`oiid`),
